f16e85794d1df2d9590f790e9c2cd85f3ca8ab4c
Tomoshige Shimomura <ii00zero1230@gmail.com>
2018-01-18 17:42:20 +0900

    複数回実行しても問題ないように.commandの確認などを追加

--- setup.sh
+++ setup.sh
@@ -1,32 +1,42 @@
 #!/bin/bash
 
+function check_command() {
+  cmd=$1
+  if ! type $cmd >/dev/null 2>&1; then
+    echo "${cmd} does not exist."
+    exit 1
+  fi
+}
+
 function install_node_exporter() {
   version=${1}
   echo "Install node exporter version ${version}"
   url="https://github.com/prometheus/node_exporter/releases/download/v${version}/node_exporter-${version}.linux-amd64.tar.gz"
-  wget -O - ${url} | tar zxvf - -C /tmp
-  cp /tmp/node_exporter-${version}.linux-amd64/node_exporter /usr/sbin/
+  wget -O - ${url} | tar zxvf - -C /tmp/
+  cp -f /tmp/node_exporter-${version}.linux-amd64/node_exporter /usr/sbin/
 }
 
 function setup_node_exporter() {
   echo "set up node_exporter"
   echo "link node_exporter.service to /etc/systemd/system/node_exporter.service"
-  ln -nfs $(pwd)/node_exporter.service /etc/systemd/system/node_exporter.service
+  cp -f node_exporter.service /etc/systemd/system/node_exporter.service
   echo "link sysconfig.node_exporter to /etc/node_exporter"
-  ln -nfs $(pwd)/sysconfig.node_exporter /etc/node_exporter
+  cp -f sysconfig.node_exporter /etc/node_exporter
   echo "reload systemd ..."
   /bin/systemctl daemon-reload
   /bin/systemctl enable node_exporter.service
   /bin/systemctl start node_exporter.service
-  /bin/systemctl status node_exporter.service -l
+  /bin/systemctl status node_exporter.service --no-pager
   echo "finish"
 }
 
 function setup_textfile_collector() {
   echo "set up textfile collector"
   textfile_collector_dir=${1}
-  # TODO: avoid dupulication
-  echo "30 * * * * root $(pwd)/text_collector/apt.sh > ${textfile_collector_dir}/apt.prom" >> /etc/crontab
+  # remove duplicate row
+  sed -i -E "/.*\.prom/d" /etc/crontab
+  # register cron
+  echo "5 * * * * root $(pwd)/text_collector/apt.sh > ${textfile_collector_dir}/apt.prom" >> /etc/crontab
   echo "5 * * * * root $(pwd)/text_collector/user.py > ${textfile_collector_dir}/user.prom" >> /etc/crontab
 }
 
@@ -38,6 +48,9 @@ function setup_cadvisor() {
   echo "finish"
 }
 
+check_command 'wget'
+check_command 'docker'
+check_command 'docker-compose'
 install_node_exporter "0.15.2"
 setup_node_exporter
 setup_textfile_collector "/var/lib/node_exporter/textfile_collector"
